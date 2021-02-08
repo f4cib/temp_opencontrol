f5fef2a546b328fa4e2a0cb27e237dae06e76dc7
f4cib <56175491+f4cib@users.noreply.github.com>
2021-02-08 07:24:13 +0100

    Add files via upload

---
+++ c.js
@@ -0,0 +1,179 @@
+// one button added for beverages Tx Set is now button 5
+// function setButton_bev added to deal with css when hovering beverages direction buttons
+var isTxEditMode = false;
+var txArray = [];
+var rxArray = [];
+var urlToArduino;
+
+var myResponseHandler = function(data) {
+
+    var completeContent = [];
+
+	completeContent = data.split('|');
+	fillArrays(completeContent[0], completeContent[1]);
+	
+	if(!isTxEditMode) 
+		setButtons(rxArray);
+	else
+	    setButtons(txArray);
+}
+
+function fillArrays(stringRx, stringTx)
+{
+	for(var i=0; i<5; i++)
+	{
+		rxArray[i] = stringRx[i].charCodeAt()-48;
+		txArray[i] = stringTx[i].charCodeAt()-48;
+	}
+}
+
+
+function setButtons(arr)
+{
+	for(var i=0; i<5; i++)
+	{		
+		setButton(i, arr[i]);
+	}	
+}
+
+function setButton(btnNr, expo)
+{
+	var buttonname = 'b'+btnNr;
+
+	if(expo == 1)
+	{
+		var onbut = document.getElementById(buttonname);
+		onbut.className = "myButton right bon";
+	};
+			
+	if(expo == 0)
+	{
+		var offbut = document.getElementById(buttonname);
+		offbut.className = "myButton right boff";
+	};
+}
+
+function setButton_bev(btnNr, expo)
+{
+	var buttonname = 'bev'+btnNr;
+
+	if(expo == 1)
+	{
+		var onbut = document.getElementById(buttonname);
+		onbut.className = "myButton_bev right bon";
+	};
+			
+	if(expo == 0)
+	{
+		var offbut = document.getElementById(buttonname);
+		offbut.className = "myButton_bev right boff";
+	};
+}
+
+function getAllContent() {
+		$.ajax({
+			crossDomain: true,
+			async : true,
+			type: "GET",
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': '*'
+			},
+			url: urlToArduino+"/Get/?callback=?",
+			dataType: 'jsonp',
+			jsonpCallback:'xx'})
+			.done(function(data){ 
+			    myResponseHandler(data.v);
+			});
+}
+
+function setContent(currentBank, toBankValue) {
+		jQuery.support.cors = true;
+
+		return $.ajax({
+			crossDomain: true,
+			async : true,
+			type: "GET",
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Headers': '*'	
+			},
+			url: urlToArduino+"/Set/"+currentBank+"/"+toBankValue,
+			contentType: "text/html",
+			dataType: "html",
+			success: function (data) { }
+		});
+}
+
+function clkButton(butNr)
+{
+	if(butNr == 5)
+	{
+		if(isTxEditMode== false)
+		{
+			setButton(butNr, 1);
+			isTxEditMode = true;
+		}
+		else
+		{
+			setButton(butNr, 0);
+			isTxEditMode = false;			
+		}
+		fetchContent();
+	}
+	else
+	{
+		if(!isTxEditMode)
+		{
+			rxArray = setRegisterArray(butNr+1, rxArray);
+			setButtons(rxArray);
+			setContent(0, rxArray.join(''));
+		}
+		else
+		{
+			txArray = setRegisterArray(butNr+1, txArray);
+			setButtons(txArray);			
+			window.setTimeout(500);
+			setContent(1, txArray.join(''));
+		}
+	}
+
+	window.setTimeout(updateLCD,100);
+}
+
+
+function verifyButtons(arri, nrOfButtons)
+{
+  var sum = 0;
+  
+  for(var u = 0;u < nrOfButtons; u++)
+    sum += arri[u];   
+    
+  return sum;
+}
+
+function updateLCD()
+{
+	createLcdString(rxArray, false);
+	createLcdString(txArray, true);
+	console.log("updateLCD");
+}
+
+// set the complete Register array to given value
+function toggleRegisterArray(regA, v1)
+{
+  for (var a = 0; a < 5; a++)
+    regA[a] = v1;
+}
+
+var myVar = setInterval(function () { fetchContent() }, 5000);
+
+function init()
+{
+    fetchContent();
+}
+
+function fetchContent() {
+        getAllContent();
+        window.setTimeout(updateLCD, 150);
+}
---
+++ f6knb.ino
@@ -0,0 +1,193 @@
+
+#if SKETCHMODE == 3
+// the welcome info. you are not allowed to change anything here! CC
+void displayWelcomeText()
+{
+  lcd.init();
+  lcd.clear();
+  lcd.home();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print(" remoteQTH.com ");
+  lcd.setCursor(0, 1);
+  lcd.print(" openMultiCon");
+}
+
+// the default start screen template
+void displayMain()
+{
+  resetDisplay();
+  lcd.print("->RX");
+  lcd.setCursor(0, 1);
+  lcd.print("  TX");
+}
+
+// set value of the pressed button into the relay register. Here you can define the mappings for the relays
+void setRegisterArray(byte button, boolean regArr[])
+{
+  if (!inTxEditMode)                    // in rx mode
+  {
+    if (button < 5)                     // only button could be active, except button 4 // F4CIB 4 replaced by 6
+    {
+      toggleRegisterArray(regArr, 0); // F4CIB one button added ie. 5 buttons, and i want only one active at once so always in this case
+
+      if (regArr[button - 1] == 1)
+        regArr[button - 1] = 0;
+      else
+        regArr[button - 1] = 1;
+    }
+    else                                // button 4: all combinations possible // F4CIB not anymore, we should never go here now
+    {
+      if (regArr[button - 1] == 0)
+        toggleRegisterArray(regArr, 1);
+    }
+  }
+  else // in tx mode
+  {
+    byte checkOne = verifyButtons(regArr, 5);   // F4CIB 4 replaced by 5, we now have 5 buttons...
+  if (checkOne == 1) // only one is on
+  {
+    if (regArr[button - 1] == 0) // was the button off before? For sure, because its only one on...
+    {
+      if (button < 5) // if its 2, turn it on.. // F4CIB again 4 replaced by 5 because now 5 buttons but button 1 is for beverage so never Tx on it... 
+        regArr[button-1] = 1;                   // F4CIB and buttons 3, 4, 5 are band dependant, let's hope the remote operator is not an idiot !
+      else
+      {
+        regArr[0] = 1;
+        regArr[1] = 1;
+        regArr[2] = 1;
+        regArr[3] = 1;
+      }
+ //     regArr[3] = 1; // if more than one is on, switch on 4, because of phasing...// F4CIB no phasing anymore last relay is an antenna so one condition up
+    }
+  }
+  
+  if (checkOne == 2) 
+  {
+    if (regArr[button - 1] == 0) // was the button off before? For sure, because its only one on...
+    {
+      if (button < 5) // if its 2, turn it on.. // F4CIB again 4 replaced by 5 because now 5 buttons but button 1 is for beverage so never Tx on it... 
+        regArr[button-1] = 1;                   // F4CIB and buttons 3, 4, 5 are band dependant, let's hope the remote operator is not an idiot !
+      else
+      {
+        regArr[0] = 1;
+        regArr[1] = 1;
+        regArr[2] = 1;
+        regArr[3] = 1;
+      }
+    }
+  }
+ 
+  if (checkOne == 3) // if 3 are on (hint: 2 can never be on, because phasing is missing.. //F4CIB hint is not valid anymore... add case checkOne == 2
+  {
+    if (regArr[button - 1] == 0) // turn last missing one on...
+      regArr[button - 1] = 1;
+    else // so now a button is switched off...
+    {
+      if (button==5) // if its 4, go back to default 1 on // F4CIB again 4 replaced by 5 because now 5 buttons but button 1 is for beverage so never Tx on it...
+      {
+        regArr[0] = 1;
+        regArr[1] = 0;
+        regArr[2] = 0;
+      }
+      else // turn the button off
+        regArr[button-1] = 0;
+      regArr[3] = 0; // phasing has to be off
+    }
+  }
+  if (checkOne == 4) // if all are on...
+  {
+    if (button < 4) // if button < 4 is pressed, just turn it off
+      regArr[button-1] = 0;
+    else // if button 4 is pressed, go to default 1 on
+    {
+      regArr[0] = 1;
+      regArr[1] = 0;
+      regArr[2] = 0;
+      regArr[3] = 0;
+    }
+  }
+  }
+}
+
+// Here you can define possible exceptions buttons/leds vs. relays. Default is 1:1
+void setRegisterLed(boolean isTx)
+{
+  if (isTx)
+  {
+    byte checkOne = verifyButtons(registersTx, 4);
+    Serial.println(checkOne);
+    for (int i = 0; i<4; i++)
+    {
+      registersTxLed[i] = registersTx[i];
+    }
+
+    if (checkOne == 3)
+      registersTxLed[3] = 0;
+  }
+  else
+  {
+    for (int i = 0; i<4; i++)
+    {
+      registersRxLed[i] = registersRx[i];
+    }
+  }
+}
+
+
+void setDisplay(boolean regArry[], byte row)
+{
+  clearLabels(row);
+  lcd.setCursor(5, row);
+
+  if (row == 0)
+  {
+    for (byte i = 3; i >= 0; i--)
+    {
+      if (regArry[i] == 1)
+      {
+        lcd.print(rxDisplayArray[i]);
+        return;
+      }   
+    }
+  }
+  else
+  {
+    int sum = 0;
+    sum = verifyButtons(regArry, 4);
+    Serial.print("su");
+    Serial.print(sum);
+
+    if (sum == 1 || sum > 2)
+    {
+      for (byte i = 3; i >= 0; i--)
+      {
+        if (regArry[i] == 1)
+        {
+          lcd.print(txDisplayArray[i]);
+          return;
+        }
+      }
+    }
+    else
+    {
+      if (regArry[0] == 1 && regArry[1] == 1)
+      {
+        lcd.print(txDisplayArray[4]);
+        return;
+      }
+      if (regArry[0] == 1 && regArry[2] == 1)
+      {
+        lcd.print(txDisplayArray[5]);
+        return;
+      }
+      if (regArry[1] == 1 && regArry[2] == 1)
+      {
+        lcd.print(txDisplayArray[6]);
+        return;
+      }
+    }
+
+  }
+}
+#endif
